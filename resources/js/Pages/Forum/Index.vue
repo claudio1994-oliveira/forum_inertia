445872e1e70724eaa6fd88c03703f25ae6b9cbfc
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-10-16 21:49:30 -0300

    inicial commit

---
+++ resources/js/Pages/Forum/Index.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import {Head} from '@inertiajs/vue3';
+import ForumLayout from "@/Layouts/ForumLayout.vue";
+import {usePage} from "@inertiajs/vue3";
+import Select from "@/Components/Select.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+
+const page = usePage();
+
+const topics = page.props.topics;
+
+</script>
+
+<template>
+    <Head title="Forum"/>
+
+    <ForumLayout>
+        <div class="sm:px-6 lg:px-8">
+            <div
+                class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+            >
+                <div class="p-6 text-gray-900">
+                    <div>
+                        <InputLabel for="select" value="Select a topic" class="sr-only"/>
+                        <Select id="select">
+                            <option value="">All topics</option>
+                            <option :value="topic.slug" v-for="topic in topics" :key="topic.id">
+                                {{ topic.name }}
+                            </option>
+                        </Select>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <template #side>
+            <div class="bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    Sidebar
+                </div>
+            </div>
+        </template>
+    </ForumLayout>
+</template>
