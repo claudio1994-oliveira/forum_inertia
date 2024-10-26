3d4e86b207b329263b97c0c558eb56dd1205d9ed
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-10-26 18:47:51 -0300

    adding more data to posts

--- resources/js/Components/Forum/Post.vue
+++ resources/js/Components/Forum/Post.vue
@@ -9,10 +9,22 @@ defineProps({
 
 <template>
     <div
-        class="block overflow-hidden bg-white shadow-sm sm:rounded-lg my-4">
-        <div class="p-6 text-gray-900 flex items-center space-x-6">
-            {{ post.body }}
+        class=" overflow-hidden bg-white shadow-sm sm:rounded-lg my-4 p-6 text-gray-900 flex items-start space-x-3">
+
+        <div class="w-7 flex-shrink-0">
+            <img :src="post.user.avatar_url" alt="avatar" class="rounded-full w-7 h-7">
         </div>
+        <div class="w-full ">
+            <div>
+                <div>{{ post.user.username }}</div>
+                <div class="text-sm text-gray-500">Posted {{ post.created_at.human }}</div>
+            </div>
+            <div class="mt-3">
+                {{ post.body }}
+            </div>
+
+        </div>
+
     </div>
 </template>
 
