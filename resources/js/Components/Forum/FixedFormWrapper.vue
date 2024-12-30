1d75f39a802cf3ab799cd3925f939982dd9eb6ca
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-12-30 14:58:05 -0300

    toogling the markdown preview

--- resources/js/Components/Forum/FixedFormWrapper.vue
+++ resources/js/Components/Forum/FixedFormWrapper.vue
@@ -1,5 +1,11 @@
 <script setup>
 
+import {ref} from "vue";
+
+const markdownPreviewEnabled = ref(false);
+const markdownPreviewHtml = ref(false);
+const markdownPreviewLoading = ref(false);
+
 </script>
 
 <template>
@@ -9,8 +15,20 @@
                 <slot name="header"/>
             </div>
 
-            <div class="mt-4">
-                <slot name="main"/>
+            <div class="mt-4 space-y-3">
+                <slot name="main" :markdownPreviewEnabled="markdownPreviewEnabled"/>
+                <div class="h-48 bg-gray-100 rounded-lg px-3 py-2 overflow-y-scroll border border-gray-300 shadow-sm"
+                     v-if="markdownPreviewEnabled" v-html="markdownPreviewHtml"
+                     :class="{ 'opacity-50': markdownPreviewLoading }"></div>
+                <div class="flex items-center justify-between">
+                    <div>
+                        markdown toolbar
+                    </div>
+                    <button class="text-sm text-indigo-500" type="button"
+                            v-on:click="markdownPreviewEnabled = !markdownPreviewEnabled">
+                        {{ markdownPreviewEnabled ? 'Turn off' : 'Turn on' }} markdown preview
+                    </button>
+                </div>
                 <div class="mt-4">
                     <slot name="button"/>
                 </div>
